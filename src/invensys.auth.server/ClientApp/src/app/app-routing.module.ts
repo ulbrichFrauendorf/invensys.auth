1b3a8e8a69087761c7d8b6c57de46bd314164a97
Ulbrich Frauendorf <invensys.za@gmail.com>
2023-09-26 09:41:29 +0200

    routing

--- src/invensys.auth.server/ClientApp/src/app/app-routing.module.ts
+++ src/invensys.auth.server/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientDetailComponent } from './clients/client-detail/client-detail.component';
+import { ClientListComponent } from './clients/client-list/client-list.component';
+import { LandingDashboardComponent } from './landing-dashboard/landing-dashboard.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingDashboardComponent},
+  {path: 'clients', component: ClientListComponent},
+  {path: 'clients/:id', component: ClientDetailComponent},
+  {path: 'users/:id', component: UserDetailComponent},
+  {path: '**', component: LandingDashboardComponent, pathMatch: 'full'},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
